885843ad955752b360dbf4320e0bf50ee1589262
Pablo <pablobarbosa0708@gmail.com>
2022-02-09 12:48:50 -0300

    final version

---
+++ contador.js
@@ -0,0 +1,21 @@
+(( )=>{
+    const minusButton = document.querySelector(".minus-button");
+    const plusButton = document.querySelector(".plus-button");
+    const counterValueSpan = document.querySelector(".counter-value");
+ 
+    minusButton.addEventListener("click", minusButtonHandler)
+    plusButton.addEventListener("click", plusButtonHandler)
+ 
+    function minusButtonHandler(){
+         let counterCurrentValue = +counterValueSpan.innerHTML
+         counterCurrentValue--
+         if(counterCurrentValue >=0){
+             counterValueSpan.innerHTML = counterCurrentValue 
+        }
+    }
+    function plusButtonHandler(){
+         let counterCurrentValue = +counterValueSpan.innerHTML
+         counterCurrentValue++
+         counterValueSpan.innerHTML =counterCurrentValue 
+    }
+ })()
